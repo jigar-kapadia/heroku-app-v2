3b13611aa67322a74adebff421c162ba90cdbe1c
Jigar Kapadia <jigark00@gmail.com>
2018-11-19 00:37:20 +0530

    issue fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions, RequestMethod } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  responseApi : any;
+  constructor(private _httpService : Http){
+
+  }
+  
+  // CallApi()
+  // {
+  // this._httpService.get('http://sunnykapadia-001-site1.itempurl.com/home/values?input=jigar')
+  // .subscribe(result => this.responseApi = result);
+  // }
 }
+
+
+
